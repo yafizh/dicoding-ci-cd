ff37571e41ac8cbe19b994709ebab6124322124a
yafizh <a.chairil.yafizh@gmail.com>
2022-09-15 21:37:07 +0800

    finish

---
+++ src/Infrastructures/database/postgres/pool.js
@@ -0,0 +1,27 @@
+/* istanbul ignore file */
+const { Pool } = require('pg');
+
+const testConfig = {
+  host: process.env.PGHOST_TEST,
+  port: process.env.PGPORT_TEST,
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  database: process.env.PGDATABASE_TEST,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const prodConfig = {
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool(prodConfig);
+module.exports = pool;
