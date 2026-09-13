cd69cc71f3683fe90f18a34998bb2951df0f9cd6
yafizh <a.chairil.yafizh@gmail.com>
2026-09-13 14:45:05 +0700

    remove ssl config for testConfig

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -7,9 +7,6 @@ const testConfig = {
   user: process.env.PGUSER_TEST,
   password: process.env.PGPASSWORD_TEST,
   database: process.env.PGDATABASE_TEST,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 };
 
 const prodConfig = {
